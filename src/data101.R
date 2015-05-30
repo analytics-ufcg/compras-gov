fc590f608bc2e3ac904a3baa833993c1053c8def
MatheusMaciel <teu.araujo@gmail.com>
2015-05-30 11:35:17 -0300

    Things!!

--- src/data101.R
+++ src/data101.R
@@ -2,3 +2,12 @@ source("util.R")
 
 data.folder <- "../data/csv/"
 
+tiposContrato.path <- paste(data.folder, "tipos_contrato.csv", sep="")
+irps.path <- paste(data.folder, "irps.csv", sep="")
+
+tiposContrato.data <- read.csv(tiposContrato.path)
+irps.data <- read.csv(irps.path)
+
+
+
+irps.pb <- irps.data %>% filter(UF.da.UASG.gerenciadora.da.IRP=="PB")
